7cac3d51e1f814018f9e5c606892121c305fdbc0
vishal-kushwaha-9912 <vishalk.ushwa9912@gmail.com>
2026-05-29 06:26:25 +0530

    first commit

---
+++ printfun.py
@@ -0,0 +1,46 @@
+# basic print
+print("hello world")
+
+# print int and float
+print(12)
+print(6.6)
+
+# print boolean
+
+print(True)
+print(False)
+
+# print multiple strings
+
+print("python ", "c++", "java", "c#", "github")
+
+print("hello i am ", "vishal kushwaha", "currently ", "i am in class", "india")
+
+# print diffent multiple items
+
+print("india", 5.6, 9, True, "vishal kushwaha", 4.99694)
+
+# print with sep .
+#
+# sep is basicly used to separte items/strings with any space or symbol.
+
+print("hello", "vishal kushwaha", "currently", "class", "india", sep="/")
+
+print("hello", "vishal kushwaha", "currently", "class", "india", sep="#")
+
+print("hello", "vishal kushwaha", "currently", "class", "india", sep="*")
+
+print("hello", "vishal kushwaha", "currently", "class", "india", sep="-")
+
+print("python ", "c++", "java", "c#", "github", sep="  ")
+
+
+# IN PRINT FUNCTION : if you want to print with a new line then you dont need any thing to write just change the line and write print function again and ir=t will print in new line .
+
+print("hello world")
+print("vishal kushwaha")
+
+#print with end 
+# end is basicly used to print in same line with any space or symbol.
+print("hello ", end="")
+print("world is here to learn python")
