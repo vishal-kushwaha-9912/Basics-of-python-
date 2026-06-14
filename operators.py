90519e8195cff666bc50313c9cf2ad6b01cf8e6a
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-15 02:11:00 +0530

    add

--- operators.py
+++ operators.py
@@ -31,7 +31,7 @@ print (x<y)
 
 print (x==y)
 print (x!=y)
-
+ 
 
 print(x>=y)
 print (x<=y) 
@@ -49,4 +49,24 @@ print (not n)
 
 
 #BITWISE OPERATORS : ITS WORK ON binary numbers.
+# is operators defined you two d/f variable have a same memory loaction is true/false.
 
+#if you see there are two value are store in a d/f variable is not means they have same memory loaction .
+
+
+a = 3
+b= 4
+print(a is b)  # output : true
+
+a = "hello-world"
+b = "hello-world"
+print(a is b) #output : false.
+
+
+
+#mebership operators
+                     #:IT is used to checked whether exist in a sequence .such as string , list , truple , set or dictinory .
+                 
+x = "India"
+print( i in x) # output: true
+print( v in x) #output : false                      
