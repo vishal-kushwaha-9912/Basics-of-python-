33c615817542b3fe89599e31bd9650fc82c1cd12
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-14 17:05:44 +0530

    add operators

--- literals.py
+++ literals.py
@@ -8,15 +8,15 @@
 
 #Number literals : it is a fixed value that represents a number. It can be an integer, a float, or a complex number.
 #example of number literals
-#a = 10       # Integer literal
-#b = 3.14     # Float literal
-#c = 2 + 3j   # Complex literal
+a = 10       # Integer literal
+b = 3.14     # Float literal
+c = 2 + 3j   # Complex literal
 
 
 
 a = 0b1010 #Binary Literals
 b = 100 #Decimal Literal
-c = 00310 #Octal Literal
+#   c = 00310 #Octal Literal
 d = 0x12c #Hexadecimal Literal
 
 #Float Literal
@@ -31,7 +31,54 @@ print(a, b, c, d)
 print(float_1, float_2,float_3)
 print(x, x.imag, x.real)
 
+""" OUTPUT : 
 10 100 200 300
 10.5 150.0 0.0015
-3.14j 3.14 0.0
+3.14j 3.14 0.0"""
 
+
+
+
+# string literals : it is a fixed value that represents a sequence of characters. It can be enclosed in single quotes, double quotes, or triple quotes.
+
+
+#example of string literals
+str1 = 'Hello, World!' # Single quotes
+str2 = "Hello, World!" # Double quotes
+str3 = '''Hello, World!''' # multiple line string literal
+unicode = u"\u00A9" # Unicode string literal print copyright symbol : u"\u00A9"
+smiley = u"\u263A" # Unicode string literal print smiley face : u"\u263A"
+raw_str = r"C:\Users\Name\Documents" # Raw string literal
+
+print(str1)
+print(str2)
+print(str3)
+print(unicode)
+print(smiley)
+print(raw_str)
+
+
+#boolean literals : it is a fixed value that represents a boolean value. It can be either True or False.
+#example of boolean literals
+bool1 = True 
+# there are sometimes is 1(true) and 0(false) in some programming languages but in python we use True and False)
+bool2 = False
+
+sum =bool1+1     # output is 2 because true is treated as 1 
+sum2 = bool2+1  # output is 1 because false is treated as 0
+print(bool1)
+print(bool2)
+print(sum)
+print(sum2)
+
+
+
+#specail literals : it is a fixed value that represents a special value. It can be None, Ellipsis, or NotImplemented.
+                
+                   ### NOTE : None is a special literal that represents the absence of a value.for an extra variable that is used in future code to declaread assign a value .
+#example of special literals
+none_literal = None
+ellipsis_literal = ...
+not_implemented_literal = NotImplemented
+print(none_literal)
+print(ellipsis_literal)
---
+++ operators.py
@@ -0,0 +1,52 @@
+#operators : it is used to perform operation on variable and value.
+
+
+#arithmetic operators
+#comparison operators
+#logical operators
+#bitwise operators
+#assigment operators
+#identity operators
+#membership operators
+
+
+#ARTHMETIC OPERATIONS
+
+x =32 
+y =25
+
+print(x + y) #add
+print(x-y) #sub
+print(x*y) #multi
+print(x/y) #dividion
+print(x%y) #modulos
+print (x**y)  #power x^y
+print(x//2)  #integer division
+
+
+#COMPARISON OPERATIONS
+
+print(x>y)
+print (x<y)
+
+print (x==y)
+print (x!=y)
+
+
+print(x>=y)
+print (x<=y) 
+
+
+#LOGICAL operators
+m = True
+n= False
+
+
+print (n or m )
+print (n and m ) 
+print (not n)  
+
+
+
+#BITWISE OPERATORS : ITS WORK ON binary numbers.
+
