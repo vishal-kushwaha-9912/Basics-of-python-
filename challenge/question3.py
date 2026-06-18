a46f9b933a32653aea960042eea85f4dfa300a82
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-18 17:27:00 +0530

    challenge

--- challenge/question3.py
+++ challenge/question3.py
@@ -17,12 +17,16 @@
 # 9
 # 10
 
+
+
 # Question 4: Print Even Numbers
+
+
 
 # Use a for loop to print all even numbers from 1 to 20.
 
-# for x in range(0,21,2):
-#     print(x)
+for x in range(0,21,2):
+    print(x)
     ##output ::
 # 0
 # 2
@@ -37,14 +41,59 @@
 # 20    
 
 
+
                         #Question 5 
+                 
+                 
                         
                         
 # Multiplication Table
 
 # Take a number from the user and print its table up to 10 using a for loop.
 
-number = int (input ("Enter the number : "))
+# number = int (input ("Enter the number : "))
 
-for i in range(1,11):
-    print(number,"*",i ,"=", number*i)
+# for i in range(1,11):
+#     print(number,"*",i ,"=", number*i)
+    
+    
+    
+    
+        #Question 6
+    
+    
+#     Print numbers from 1 to 20 using a for loop and use if-else to print:
+
+# "Even" if the number is even
+# "Odd" if the number is odd
+
+for num in range(1,21):
+  
+    if num%2 ==0:
+        print(num,"- even")
+    else:
+        print(num ,"- odd")
+        
+        
+        
+  ##output::      
+# 1 - odd
+# 2 - even
+# 3 - odd
+# 4 - even
+# 5 - odd
+# 6 - even
+# 7 - odd
+# 8 - even
+# 9 - odd
+# 10 - even
+# 11 - odd
+# 12 - even
+# 13 - odd
+# 14 - even
+# 15 - odd
+# 16 - even
+# 17 - odd
+# 18 - even
+# 19 - odd
+# 20 - even
---
+++ challenge/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+ # if i%2=0:
+    #     print("even")
+    # else:
+    #     print("odd")
