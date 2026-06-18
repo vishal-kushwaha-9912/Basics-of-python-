c73015a83f7d0057231e0dca4069f1433fce7ab9
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-18 15:37:40 +0530

    for loop

--- forloop.py
+++ forloop.py
@@ -30,5 +30,48 @@
 
  
 
+EASY way to understand forloop
 
 
+
+
+
+
+If you want the robot to say "Hello" 5 times, you could write:
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+
+
+
+
+#A for loop tells the robot:
+
+"Repeat this job again and again."
+
+Example ::
+    
+for i in range(5):
+ print("Hello")
+
+Output:
+
+Hello
+Hello
+Hello
+Hello
+Hello
+
+
+## Breaking it down :
+
+for i in range(5):
+    
+for = start repeating
+i = a counter (keeps track of how many times)
+range(5) = repeat 5 times
+: = "the repeated work starts here"
