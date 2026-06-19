2636975f52ce02968f0a4587b34f81c0fd79cb19
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-19 23:46:47 +0530

    day2

--- challenge/DAY 2/question1.py
+++ challenge/DAY 2/question1.py
@@ -55,10 +55,71 @@
 
 # Write a program that uses a for loop to find the sum of all even numbers from 1 to 20.
 # Expected output: 110 (2+4+6+8+10+12+14+16+18+20)
-
-for x in range(0,21):
+# total=0
+# for x in range(0,21):
   
-  if x%2==0:
-    total=0
-    total = total + x
-    print (total )
+#   if x%2==0:
+    
+#     total = total + x
+    
+# print("the sum of all even numbers is  : ", total )
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+# #Question 5: While Loop + If-Else (Hard)
+# Simple guessing game
+
+# Write a program where:
+
+# Computer "thinks" of a number between 1-10
+# User guesses the number (using input)
+# Program tells if guess is too high, too low, or correct
+# Loop continues until user guesses right
+
+
+#ANSWER :::
+
+# import random
+
+# jackpot = random.randint(1,10)
+
+# guess = int(input("Enter the number : "))
+
+
+# while guess != jackpot:
+#     if guess <jackpot:
+#         print("Guess Higher")
+#     else:
+#         print("Guess Lower")
+#     guess =int (input("Guess Number is :"))
+    
+# print("🎉 Correct Guess ")   
+
+
+##### IMPROVE VERSION ^^^^^
+
+import random
+
+jackpot = random.randint(1,10)
+count = 1
+guess = int(input("Enter the number : "))
+
+
+while guess != jackpot:
+    if guess <jackpot:
+        print("The Number : ",guess)
+        print("Guess Higher")
+    else:
+        print("The Number : ",guess)
+        print("Guess Lower")
+    guess =int (input("Guess Number is :"))
+    count +=1
+print("🎉 Correct Guess ")   
+print("Total Attempt to Guess the Number : ", count)
+print("The number was:", jackpot)
