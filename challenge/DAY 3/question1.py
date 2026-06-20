e4a87316853a79e71e66df81d51a8e78623bdc75
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-20 15:23:40 +0530

    DAY 3

---
+++ challenge/DAY 3/question1.py
@@ -0,0 +1,17 @@
+# Question 2: For Loop + If-Else (Medium)
+# Find prime numbers
+
+# Write a program that finds and prints all prime numbers between 1 and 20.
+# (Hint: A prime number is only divisible by 1 and itself)
+
+for x in range(2,21):
+    is_prime = True
+    
+    for i in range(2,x):
+        if x % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(x)    
+    
+         
