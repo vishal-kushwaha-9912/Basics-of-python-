4f0d872c85e8f242032c0f6921cd7ea86137ff47
Your Name <vishalk.ushwa9912@gmail.com>
2026-07-04 11:35:25 +0530

    forloop update

--- forloop/questions1.py
+++ forloop/questions1.py
@@ -5,7 +5,10 @@
 # ***
 # ****
 # *****
-
+# for x in range(1,7):
+#     for y in range(1,x):
+#         print("*",end=" ")
+#     print()
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # 22. Print this pattern.
 # *****
@@ -14,6 +17,11 @@
 # **
 # *
 
+
+# for x in range(6,1,-1):
+#     for y in range(1,x):
+#         print("*",end=" ")
+#     print()
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
@@ -24,6 +32,10 @@
 # 1234
 # 12345
 
+# for x in range(1,6):
+#     for y in range(1,x+1):
+#         print(y,end=" ")
+#     print()
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # 24. Print this pattern.
@@ -33,6 +45,10 @@
 # 4444
 # 55555
 
+# for x in range(1,6):
+#     for y in range(0,x):
+#         print(x,end=" ")
+#     print()
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -42,6 +58,23 @@
 # ABC
 # ABCD
 # ABCDE
+
+for x in range(1,6):
+    for y in range(0,x):
+        print(chr(y+65),end="")
+    print()
+    
+    
+# Logic to remember
+
+# For alphabet patterns:
+
+# chr(65 + y) → Prints A B C D...
+# chr(97 + y) → Prints a b c d...
+# Pattern Formula
+# Print increasing alphabets: chr(65 + y)
+# Print the same alphabet repeatedly: chr(64 + x)
+
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
