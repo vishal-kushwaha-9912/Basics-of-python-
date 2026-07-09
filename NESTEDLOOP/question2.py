38365eea6a60fe61a52f25022672c4470b3680a7
Your Name <vishalk.ushwa9912@gmail.com>
2026-07-09 07:12:49 +0530

    upadtes nested loop

--- NESTEDLOOP/question2.py
+++ NESTEDLOOP/question2.py
@@ -7,13 +7,13 @@
 # *****
 
 
-for x in range (1,6):
-    space = 5-x
-    for y in range(0,space):
-        print("" , end=" ")
-    for y in range(0,x):
-        print("*",end="")
-    print()
+# for x in range (1,6):
+#     space = 5-x
+#     for y in range(0,space):
+#         print("" , end=" ")
+#     for y in range(0,x):
+#         print("*",end="")
+#     print()
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -30,13 +30,13 @@ for x in range (1,6):
 
 
 
-for x in range (1,10):
-    space = 9 -x
-    for y in range(0, space):
-        print("",end=" ")
-    for y in range(1,x):
-        print("*",end=" ")
-    print()
+# for x in range (1,10):
+#     space = 9 -x
+#     for y in range(0, space):
+#         print("",end=" ")
+#     for y in range(1,x):
+#         print("*",end=" ")
+#     print()
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -54,13 +54,13 @@ for x in range (1,10):
 
 
 
-for x in range (5,0,-1):
-    space = 5-x
-    for y in range(0,space):
-        print("",end=" ")
-    for y in range(0,x):
-        print("*",end=" ")
-    print()
+# for x in range (5,0,-1):
+#     space = 5-x
+#     for y in range(0,space):
+#         print("",end=" ")
+#     for y in range(0,x):
+#         print("*",end=" ")
+#     print()
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -80,21 +80,21 @@ for x in range (5,0,-1):
 
 
 
-for x in range (1,10,2):
-    space = (9-x) // 2
-    for y in range(space):
-        print(" ",end="")
-    for y in range(x):
-        print("*",end="")
-    print()
+# for x in range (1,10,2):
+#     space = (9-x) // 2
+#     for y in range(space):
+#         print(" ",end="")
+#     for y in range(x):
+#         print("*",end="")
+#     print()
 
-for x in range (7,0,-2):
-    space = (9-x)//2
-    for y in range(space):
-        print(" ",end="")
-    for y in range(x):
-        print("*",end="")
-    print()
+# for x in range (7,0,-2):
+#     space = (9-x)//2
+#     for y in range(space):
+#         print(" ",end="")
+#     for y in range(x):
+#         print("*",end="")
+#     print()
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
@@ -104,12 +104,15 @@ for x in range (7,0,-2):
 # 4 5 6
 # 7 8 9 10
 # 11 12 13 14 15
-num = 1 
-for x in range(1,6):
-    for y in range(1,x+1):
-        print(num,end=" ")
-        num += 1
-    print()
+
+
+
+# num = 1 
+# for x in range(1,6):
+#     for y in range(1,x+1):
+#         print(num,end=" ")
+#         num += 1
+#     print()
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -144,6 +147,13 @@ for x in range(1,6):
 # X O X O X O
 # O X O X O X
 
+for x in range(1,7):
+    for y in range(1,7):
+        if (x+y)%2 ==0 :
+         print("X", end=" ")
+        else:
+            print("O", end=" ")
+    print()
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
